df08cac4b63c14989ff6d774d1c0ac25655e898a
Heeyoung Yang <dev.heeyoung@gmail.com>
2021-12-23 23:29:28 -0500

    added displayItems
    created function for displaying items

--- src/main.js
+++ src/main.js
@@ -5,6 +5,23 @@ function loadItems() {
     .then(json => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    const html = items.map(item => createHTMLString(item));
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+        <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+            <span class="item__description">${item.genger}, ${item.size}</span>
+        </li>
+    `;
+}
+
 // main
 loadItems()
     .then(items=> {

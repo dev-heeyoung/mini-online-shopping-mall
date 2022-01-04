ed61bd191a2604b1ac1cd88d24c8663ca3fdd573
Heeyoung Yang <dev.heeyoung@gmail.com>
2022-01-04 15:45:19 -0500

    added Event Listeners

--- src/main.js
+++ src/main.js
@@ -17,17 +17,39 @@ function createHTMLString(item) {
     return `
         <li class="item">
             <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
-            <span class="item__description">${item.genger}, ${item.size}</span>
+            <span class="item__description">${item.gender}, ${item.size}</span>
         </li>
     `;
+}
+
+// Handle button click
+function onButtonClick(event, items) {
+    const target = event.target;
+    const key = target.dataset.key;
+    const value = target.dataset.value;
+
+    if(key == null || value == null){
+        return;
+    }
+    
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+}
+
+// Set event listeners for logo and buttons
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', (event) => onButtonClick(event, items));
 }
 
 // main
 loadItems()
     .then(items=> {
         console.log(items);
-        //displayItems(items);
-        //setEventListeners(items);
+        displayItems(items);
+        setEventListeners(items);
     })
     .catch(console.log)
-    
+
